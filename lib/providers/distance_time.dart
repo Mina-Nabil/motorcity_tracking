16c432c479f547622a63b6e9efd4f84837ce96cb
Mina Nabil <mina9492@hotmail.com>
2020-04-11 02:08:00 +0200

    Senior Assist

--- lib/providers/distance_time.dart
+++ lib/providers/distance_time.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import "package:http/http.dart" as http;
 
 const DISTANCE_TIME_GOOGLE_API_KEY = 'AIzaSyAN3Ba5-MW6y8g2eyL57Ls9IFDDC9mwf8E';
@@ -6,7 +8,11 @@ class GoogleDistanceTime {
 
   static Future<String> getResponse (double startLatt,double startLong,double endLatt,double endLong) async{
     final request = await http.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=$startLatt,$startLong&destinations=$endLatt,$endLong&key=$DISTANCE_TIME_GOOGLE_API_KEY');
-    print("${request.body}");
+    dynamic body = jsonDecode(request.body);
+
+    print(body["rows"][0]["elements"][0]["distance"]["text"]);
+    print(body["rows"][0]["elements"][0]["duration"]["text"]);
+
     return request.body;
   }
   
